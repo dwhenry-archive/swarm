67f1fc5ecb797b91f9f3f8d3787180fa4c6bf0d4
David Henry <dw_henry@yahoo.com.au>
2012-05-23 00:15:26 +0100

    move top level spec to appropriate location..

--- spec/swarm_spec.rb
+++ spec/swarm_spec.rb
@@ -1,4 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Swarm" do
+  it 'single passing spec' do
+    `bundle exec rake swarm:specs[spec/single_pass] NUM_DRONES=1 `.should =~ /^\.\n/
+  end
+
+  it 'single failing spec' do
+    `bundle exec rake swarm:specs[spec/single_fail] NUM_DRONES=1`.should =~ /^1 failed:\n/
+  end
 end
